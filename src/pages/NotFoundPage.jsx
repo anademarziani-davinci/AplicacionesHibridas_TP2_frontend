83550835f0485d2ac84aed9fd4d55f9a6d95eb94
Ana Maria Demarziani <ana.demarziani@davinci.edu.ar>
2026-08-02 23:44:48 -0300

    Cambio pagina not found

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router"
 export default function NotFoundPage() {
     return (
         <div className="container mt-5 text-center">
-            <h1 className="display-1 fw-bold">404</h1>
+            <h2 className="display-1 fw-bold">404</h2>
             <p className="fs-4 text-muted mb-4">La página que buscás no existe.</p>
-            <Link to="/" className="btn btn-primary">Volver</Link>
+            <Link to="/" className="btn btn-dark w-100 py-2 mt-2 fw-semibold ">Volver</Link>
         </div>
     )
 }
